b60fece18af4c4a9fab28a61b7ec05b8d54d80dd
Abhishek9907 <abhishekmishra7766@gmail.com>
2023-12-20 11:18:03 +0000

    Variable File Added

---
+++ 01_basic/variable.js
@@ -0,0 +1,11 @@
+const accountId=122233
+let accountEmail="abhishek@google.com"
+var accountPassword=123
+accountCity="Raipur"
+let accountState;
+
+accountEmail="newemail@gmail.com"
+accountPassword=321
+accountCity="Bhilai"
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState,accountId])
