ed6c2ba2da29854e8e20a641307d5b7b70ed20dc
Abhishek9907 <abhishekmishra7766@gmail.com>
2023-12-20 12:40:08 +0000

    Operator Added

--- 01_basic/03_typeconversion.js
+++ 01_basic/03_typeconversion.js
@@ -1,10 +1,10 @@
-let score ="33"
+// let score ="33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let scoreNumber=Number(score)
-console.log(scoreNumber)
+// let scoreNumber=Number(score)
+// console.log(scoreNumber)
 
 //"33"=>33
 //"33abc"=>Nan
@@ -16,3 +16,31 @@ console.log(scoreNumber)
 //1,0=>true,false
 //""=>false
 //"somevalue"=>true
+
+
+/*######################### Operations ####################*/
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2%2)
+//console.log(2+2)
+
+let str1="hi"
+let str2=" Abhishek"
+// console.log(str1+str2);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+//console.log(++gameCounter);
