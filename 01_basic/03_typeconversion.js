5a546ca4fa2351336cc8dc8cafdba6f808768d41
Abhishek9907 <abhishekmishra7766@gmail.com>
2023-12-20 12:05:06 +0000

    typeconversion in js file created

---
+++ 01_basic/03_typeconversion.js
@@ -0,0 +1,18 @@
+let score ="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let scoreNumber=Number(score)
+console.log(scoreNumber)
+
+//"33"=>33
+//"33abc"=>Nan
+//null=>0
+//undefine=>undefine
+//bool=>1,0
+
+/*Boolean Conversion*/
+//1,0=>true,false
+//""=>false
+//"somevalue"=>true
