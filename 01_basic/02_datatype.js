4014e49161e8fda663c1ce24ab936ff7088b8fe9
Abhishek9907 <abhishekmishra7766@gmail.com>
2023-12-20 11:50:31 +0000

    Datatype File Created

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name="user"
+let age=23
+let isLogged=true
+
+//null =>standalone value
+//undefine
+//int
+//string
+//boolean
+//symbol =>uniques
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
