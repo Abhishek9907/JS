ee531daf31567f369f810df5150cdf6a5ca5bd4f
Abhishek9907 <abhishekmishra7766@gmail.com>
2023-12-22 10:00:33 +0000

    some more methods of array

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -6,4 +6,15 @@ const allHero=marvel_superhero.concat(dc_superhere)
 //console.log(allHero);
 
 const all_new_hero=[...allHero,...dc_superhere]
-console.log(all_new_hero);
+// console.log(all_new_hero);
+
+console.log(Array.isArray("Abhishek"));
+console.log(Array.from("Abhishek"));
+console.log(Array.from({name:"Abhishek"})); //Intersting
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
